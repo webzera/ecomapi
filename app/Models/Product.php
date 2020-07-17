9def1d43245ef2b5adff6c0bb4777f4863a85710
webzera <esjohnzen@gmail.com>
2020-07-17 15:56:36 +0530

    make relationships with models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+    	return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Review extends Model
 {
-    //
+    public function product(){
+    	return $this->belongsTo(Product::class);
+    }
 }
