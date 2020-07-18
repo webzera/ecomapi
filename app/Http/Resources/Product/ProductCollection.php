8b7a573be35611d819726a299a969d746ed21860
webzera <esjohnzen@gmail.com>
2020-07-18 15:38:58 +0530

    Product collection Resources added

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,            
+            'ourPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(),1) : 'No rate yet',
+            'href' => [
+                'link' => route('products.show', $this->id) 
+            ],
+        ];
+        // return parent::toArray($request);
     }
 }
